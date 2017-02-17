ccde9e27799014bfd977752b4f0f9564a246779e
Norman Ettedgui <nettedgu@outlook.com>
2017-02-16 17:45:42 -0800

    employee table creation and data population .sql

---
+++ employees.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employees (
+	email varchar(50) primary key,
+	password varchar(20) not null,
+	fullname varchar(100)
+);
+
+INSERT INTO movies VALUES(
+	'classta@email.edu', 
+	'classta',
+	'TA CS122B'
+);
+
